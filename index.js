5f7b08b1f44c33d5fa35f27f78be4b051cdb3f06
ANDREW\cweidya <chrisweidya@hotmail.com>
2017-04-23 13:11:08 -0400

    new deployment build

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express'),
   app = express(),
-  Routes = require('./js/routes.js');
+  Routes = require('./js/routes.js'),
+  cors = require('cors'),
+  bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.NODE_PORT || 3000));
+
+app.use(cors());
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use('/', express.static(__dirname));
+
+app.get('/generateDummyData', Routes.generateDummyData);
+app.post('/addUserAction', Routes.addUserAction);
+app.get('/updateUserInfo', Routes.updateUserInfo);
+app.get('/userActions', Routes.getUserActions);
+app.get('/createUser', Routes.createUser);
 
 app.get('/', Routes.game);
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,26 @@ var phaser = path.join(phaserModule, 'build/custom/phaser-split.js'),
   pixi = path.join(phaserModule, 'build/custom/pixi.js'),
   p2 = path.join(phaserModule, 'build/custom/p2.js');
 
-module.exports = {
-  // module: {
-  //       rules: [
-  //           { test: /pixi\.js/, loader: "script-loader" },
-  //           { test: /p2\.js/, loader: "script-loader" },
-  //           { test: /\.json$/, loader: 'json-loader' },
-  //       ]
-  //   },
-  //   resolve: {
-  //       alias: {
-  //           'phaser': phaser,
-  //           'pixi.js': pixi,
-  //           'p2': p2,
-  //       }
-  //   },
-    context: __dirname,
-    entry: __dirname + "/js/main.js",
-    output: {
-        path: __dirname + "/js/dist/",
-        filename: "bundle.js"
-    }
+var commonConfig = {
+  context: __dirname,
+  entry: __dirname + "/js/main.js",
+  output: {
+    path: __dirname + "/js/dist/",
+    filename: "bundle.js"
+  }
+};
+
+productionConfig = function() {
+  return commonConfig;
+}
+
+developmentConfig = function() {
+  return commonConfig;
+}
+
+module.exports = function(env) {
+  if (env === 'production') {
+    return productionConfig();
+  }
+  return developmentConfig();
 }
